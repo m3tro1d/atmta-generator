75d7c9361b099f7ff85816d0e9a688772c0d9018
m3tro1d <qazwf734@gmail.com>
2020-07-18 20:18:56 +0300

    Add actual arguments

--- atmta-generator.py
+++ atmta-generator.py
@@ -7,6 +7,15 @@ import os
 parser = argparse.ArgumentParser(
 	description="""This script is (pretty much useless) generates ATMTA pictures, flipped by the specified side.""")
 
+parser.add_argument("SIDE", choices=["right", "left"],
+	help="image will be flipped with this side")
+
+parser.add_argument("FILE",
+	help="file name of the picture")
+
 args = parser.parse_args()
+side = args.SIDE
+filename = args.FILE
+
 
 # Process the specified file with ffmpeg
