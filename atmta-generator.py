c6933f7b336c958ffd17d3e7196d435e379de78d
m3tro1d <qazwf734@gmail.com>
2020-07-18 20:24:08 +0300

    Add some useful functions

--- atmta-generator.py
+++ atmta-generator.py
@@ -3,6 +3,16 @@ import ffmpeg
 import os
 
 
+def get_name(filename):
+	"""Returns file's name w/o extension"""
+	return ".".join(filename.split(".")[:-1])
+
+
+def get_ext(filename):
+	"""Returns file's extension"""
+	return filename.split(".")[-1]
+
+
 # Parse the input parameters
 parser = argparse.ArgumentParser(
 	description="""This script is (pretty much useless) generates ATMTA pictures, flipped by the specified side.""")
