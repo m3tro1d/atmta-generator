886715c468e7c3a1816cebcc4322e268ac725d34
m3tro1d <qazwf734@gmail.com>
2020-07-18 20:14:34 +0300

    Add argument parser

--- atmta-generator.py
+++ atmta-generator.py
@@ -3,6 +3,10 @@ import ffmpeg
 import os
 
 
-# Parse input arguments
+# Parse the input parameters
+parser = argparse.ArgumentParser(
+	description="""This script is (pretty much useless) generates ATMTA pictures, flipped by the specified side.""")
+
+args = parser.parse_args()
 
 # Process the specified file with ffmpeg
