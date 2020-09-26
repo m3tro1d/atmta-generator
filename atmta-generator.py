3f978ef883f05fa13e3cbb5d9fddd616b06e415a
m3tro1d <qazwf734@gmail.com>
2020-09-26 15:43:48 +0300

    Formatting & minor edits

--- atmta-generator.py
+++ atmta-generator.py
@@ -16,9 +16,8 @@ def get_ext(filename):
 def output_name(filename):
     """Returns a nicely formatted output filename"""
     return "{}-{}flipped.{}".format(get_name(filename),
-        side,
-        get_ext(filename)
-    )
+                                    side,
+                                    get_ext(filename))
 
 
 # Parse the input parameters
@@ -26,11 +25,12 @@ parser = argparse.ArgumentParser(
     description="""This script is (pretty much useless) generates ATMTA
     pictures, flipped by the specified side.""")
 
-parser.add_argument("SIDE", choices=["right", "left"],
-    help="image will be flipped with this side")
+parser.add_argument("SIDE",
+                    choices=["right", "left"],
+                    help="image will be flipped with this side")
 
 parser.add_argument("FILE",
-    help="file name of the picture")
+                    help="name of the picture file")
 
 args = parser.parse_args()
 side = args.SIDE
